0ba2e83e9e9ab784c546c0e3feeda4a131f951f0
index00913 <jisu00913@gmail.com>
2023-09-25 15:39:35 +0900

    useSatate를 통한 state변경으로 화면 잰더링

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,21 @@ import './style.scss';
 //리액트는 원본이 있어야 본사본을 통해서 차이점을 비교분석
 //리액트안에서 배열이나, 객체같은 참조형 자료는 무조건 deep copy를 해서 데이터를 변경해야됨
 
+import { useState } from 'react';
+
 function App() {
-	let isPop = true;
-	let isFooter = true;
+	console.log('app');
+	//const [상태값, 상태변경전용함수] = useState(초기값);
+	//리액트 컴포넌트는 state값이 State변경함수로 변경되야지만 컴포넌트가 재랜더링됨
+	//숫자를 증가, 감소 기킬때 전위증감 연산자를 써야지만 해당 렌더링 사이클에서 바로 값이 변경되면서 다음번 렌더링에 반영됨
+	let [Num, setNum] = useState(0);
+	console.log(Num);
+
 	return (
 		<>
-			<Header />
-			{isFooter && <Footer />}
-			{isPop ? <Popup /> : null}
-			<List />
+			<h1>{Num}</h1>
+			<button onClick={() => setNum(--Num)}>minus</button>
+			<button onClick={() => setNum(++Num)}>plus</button>
 		</>
 	);
 }
@@ -43,7 +49,9 @@ export default App;
 	useRef - 컴포넌트 안쪽에서 특정 값을 참조객체에 담을때
 
 	리액트의 성능관를 hook
-	리액트에서의 memoization
+	리액트에서의 memoization - 메모리점유율을 늘려서 성능을 개선
+	자바스크립트는 기본적으로 Garbage collector에 의해서 메모리가 관리됨
+	아래의 hook을 통해서 특정 값을 강제 메모이제이션 처리하면 가비지컬렉터에서 제외함
 	memo(컴포넌트 자체를 메모이제이션)
 	useCallback (컴포넌트 안쪽의 핸들러 함수 자체를 메모이제이션)
 	useMemo (특정 핸들러함수의 리턴값을 메모이제이션)
