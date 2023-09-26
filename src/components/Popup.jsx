0361cbf5ffa8877444f8e52b1b1e2261715ac8bc
index00913 <jisu00913@gmail.com>
2023-09-26 09:45:17 +0900

    팝업 안에 숫자 증가 이벤트

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from 'react';
+
 function Popup() {
-	return <aside>Popup</aside>;
+	const [Num, setNum] = useState(useState);
+	useEffect(() => {
+		//컴포넌트가 mount시 한번만 실행
+		console.log('popup 생성');
+
+		return () => {
+			//컴포넌트가 unmount시 한번만 실행
+			console.log('popup소멸');
+		};
+	}, []);
+
+	return (
+		<aside>
+			<h1>{Num}</h1>
+			<button onClick={() => setNum(Num - 1)}>minus</button>
+			<button onClick={() => setNum(Num + 1)}>plus</button>
+		</aside>
+	);
 }
 
 export default Popup;
