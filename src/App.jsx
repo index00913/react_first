e0de1bda2928e2f3baf4928507554c37c3b31e2b
index00913 <jisu00913@gmail.com>
2023-09-26 09:30:02 +0900

    팝업 열기 수정

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
 	const [Open, setOpen] = useState(false);
 	return (
 		<>
-			<button onClick={setOpen(true)}>팝업 열기</button>
-			<button onClick={setOpen(false)}>팝업 닫기</button>
+			<button onClick={() => setOpen(true)}>팝업 열기</button>
+			<button onClick={() => setOpen(false)}>팝업 닫기</button>
 			{Open && <Popup />}
 		</>
 	);
